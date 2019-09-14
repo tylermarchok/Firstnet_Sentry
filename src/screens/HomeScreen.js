0a77440e8073faa168254778a112e18c6e92b24b
Tyler Marchok <tylermarchok@Tylers-MacBook-Pro.local>
2019-09-14 12:53:44 -0400

    rand event selection

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -63,8 +63,10 @@ export default function HomeScreen() {
         </View>
       </ScrollView>
 
+       int randNum = Math.randNum(1, 3)*10.ceil()
+
       <text>
-      var str = JSON.stringify(obj, null, 1);
+      var str = JSON.stringify(ID.randNum, null, 1);
       </text>
 
       <View style={styles.tabBarInfoContainer}>
